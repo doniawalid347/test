d7f6dcd11e70a076b247bfbf8f38c21cfdde4730
doniawalid347 <doniawalid347@gmail.com>
2025-08-14 18:10:21 +0300

    js

---
+++ 14-8/main.js
@@ -0,0 +1,22 @@
+async function basicFetch() {
+  const output = document.getElementById("basicOutput");
+  try {
+    const response = await fetch("https://dummyjson.com/products").then(res => res.json());
+    const products = response.products;
+
+
+    // نضيف كل المنتجات ككروت
+    products.forEach(product => {
+      output.innerHTML += `
+        <div class="card">
+          <img src="${product.thumbnail}" alt="${product.title} Image">
+          <h3>${product.title}</h3>
+          <p class="price">$${product.price}</p>
+          <p>${product.description}</p>
+        </div>
+      `;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
